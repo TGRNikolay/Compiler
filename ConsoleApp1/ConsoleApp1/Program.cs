bac09129504a9e9731ab0b524342bcd94fd480ea
TGRnikolay <vasya.pupkin_no.00@mail.ru>
2025-09-29 20:40:01 +0300

    Изменения кода

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int result = 2 + 2;
+            Console.WriteLine($"2 + 2 = {result}");
         }
     }
 }
